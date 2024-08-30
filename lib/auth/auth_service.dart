3d217530cad6d885177785fe5214550b51389f60
Yekaterina Korobeinikova <kate.korobeinikova@gmail.com>
2024-08-31 01:40:04 +0500

    logout

--- lib/auth/auth_service.dart
+++ lib/auth/auth_service.dart
@@ -18,6 +18,9 @@ class AuthService {
   // sign up
 
   // sign out
+  Future<void> signOut() async {
+    return await _auth.signOut();
+  }
 
   // errors
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_minimal_chat_app/auth/auth_service.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  void logout() {
+    // get auth service
+    final _authService = AuthService();
+
+    _authService.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home"),),
+      appBar: AppBar(
+        title: Text("Home"),
+        actions: [
+          // logout button
+          IconButton(onPressed: logout, icon: Icon(Icons.logout))
+        ],
+      ),
     );
   }
 }
