df8984358c3ee8ad6fb58c2bc12f05fabdcf3112
Yekaterina Korobeinikova <kate.korobeinikova@gmail.com>
2024-08-30 20:34:30 +0500

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_minimal_chat_app/auth/login_or_register.dart';
+import 'package:flutter_firebase_minimal_chat_app/firebase_options.dart';
 import 'package:flutter_firebase_minimal_chat_app/themes/light_mode.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MainApp());
 }
 
